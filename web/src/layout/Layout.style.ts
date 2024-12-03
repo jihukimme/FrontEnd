1de9422a43be6e39dc26d1920a61af20aa88e5c2
jihukimme <jihu0210@naver.com>
2024-12-03 12:53:03 +0900

    feat: layout 설정
    width가 768px 미만일 시 컴포넌트 배치를 세로로

--- web/src/layout/Layout.style.ts
+++ web/src/layout/Layout.style.ts
@@ -8,4 +8,11 @@ export const ContentWrapper = styled.main`
   min-height: calc(100vh - 8vh);
   overflow-y: auto;
   padding: 0 20px;
+
+  /* 768px 이하 화면에서 모바일 환경에 최적화 */
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
 `;
